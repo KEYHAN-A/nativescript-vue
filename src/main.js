4b00bf4b386d88b4a99b871572a89fba409464ff
Keyhan <keyhan.igo@gmail.com>
2019-09-20 17:22:49 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import App from '~/App.vue';
+import { Page, ActionBar, GridLayout, Button, Img, Label } from 'nativescript-vue-web';
+import router from './router';
+import store from './store';
+
+Vue.component('Page', Page);
+Vue.component('ActionBar', ActionBar);
+Vue.component('GridLayout', GridLayout);
+Vue.component('Button', Button);
+Vue.component('Img', Img);
+Vue.component('Label', Label);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount('#app');
